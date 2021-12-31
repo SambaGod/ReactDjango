a5e647a24766bf66aa022e6a06fea0a3f0d60220
SambaGod <waleed.gatosorriso@gmail.com>
2021-12-31 12:04:33 +0200

    Initial setup

---
+++ bwf-api/api/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Group(models.Model):
+    name = models.CharField(max_length=32, null=False, unique=False)
+    location = models.CharField(max_length=32, null=False)
+    description = models.CharField(max_length=256, null=False)
+     
+    class Meta:
+        unique_together = (( 'name', 'location' ))
