337f6de090ccdc25aee89501ff3a4693f68b3223
SambaGod <waleed.gatosorriso@gmail.com>
2022-01-02 17:43:10 +0200

    Conditional serializer added to Group viewset

--- bwf-api/api/views.py
+++ bwf-api/api/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
 from .models import Group, Event
 from rest_framework import viewsets
-from .serializers import GroupSerializer, EventSerializer
+from .serializers import GroupSerializer, GroupFullSerializer, EventSerializer
+from rest_framework.response import Response
 
 class GroupViewset(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+    
+    # GroupFullSerializer fired only if specific group is retrieved
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = GroupFullSerializer(instance, many=False, context={ 'request': request })
+        return Response(serializer.data)
 
 class EventViewset(viewsets.ModelViewSet):
     queryset = Event.objects.all()
