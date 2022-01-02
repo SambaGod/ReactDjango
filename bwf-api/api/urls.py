9e710920cce291f62a265243eb88d873d7ccd663
SambaGod <waleed.gatosorriso@gmail.com>
2022-01-02 17:36:14 +0200

    Added Event model and migration

--- bwf-api/api/urls.py
+++ bwf-api/api/urls.py
@@ -4,6 +4,7 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'groups', views.GroupViewset)
+router.register(r'events', views.EventViewset)
 
 urlpatterns = [
     re_path(r'^', include(router.urls)),
--- bwf-api/api/views.py
+++ bwf-api/api/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from .models import Group
+from .models import Group, Event
 from rest_framework import viewsets
-from .serializers import GroupSerializer
+from .serializers import GroupSerializer, EventSerializer
 
 class GroupViewset(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+class EventViewset(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
